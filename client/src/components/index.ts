03b0d516b718e25e3aa74cf6671de783ec0396eb
NjimsRane <njims2011@gmail.com>
2023-06-05 12:41:00 +0100

    change made on import

--- client/src/components/index.ts
+++ client/src/components/index.ts
@@ -1,26 +1,24 @@
-import RandomStars from './randomStars/RandomStars';
-
-export { default as Navbar } from './navbar/Navbar';
+export { default as Navbar } from "./navbar/Navbar";
 
 // home
-export { default as HomeProducts } from './homeComponents/HomeProducts'
-export { default as HomeProduct } from './homeComponents/HomeProduct'
-export { default as Brands } from './brands/Brands'
+export { default as HomeProducts } from "./homeComponents/HomeProducts";
+export { default as HomeProduct } from "./homeComponents/HomeProduct";
+export { default as Brands } from "./brands/Brands";
 
 // stars
-export { default as RandomStars } from './randomStars/RandomStars'
+export { default as RandomStars } from "./randomStars/RandomStars";
 
 // blog
-export { default as BlogTitle } from './blogPost/BlogTitle' 
-export { default as BlogPost } from './blogPost/BlogPost' 
-export { default as BlogGallery } from './blogPost/BlogGallery' 
+export { default as BlogTitle } from "./blogPost/BlogTitle";
+export { default as BlogPost } from "./blogPost/BlogPost";
+export { default as BlogGallery } from "./blogPost/BlogGallery";
 
 // hero
-export { default as Hero } from './hero/Hero';
-export { default as HeroComponents } from './hero/HeroComponents';
+export { default as Hero } from "./hero/Hero";
+export { default as HeroComponents } from "./hero/HeroComponents";
 
 // all the product by id
-export { default as ProductItem } from './products/ProductItem';
+export { default as ProductItem } from "./products/ProductItem";
 
 // footer
-export { default as FooterComponent } from './footerComponents/FooterComponent'
+export { default as FooterComponent } from "./footerComponents/FooterComponent";
